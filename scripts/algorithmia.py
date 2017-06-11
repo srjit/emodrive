81a3ae396db33cfc0d7511997b2a67c897e452fd
Sreejith Sreekumar <sreejith2904@gmail.com>
2017-06-11 17:31:12 -0400

    api scripts

---
+++ scripts/algorithmia.py
@@ -0,0 +1,23 @@
+import Algorithmia
+
+apiKey = 'simQAbMR5x/Aw1QuWcRenMEWffU1'
+client = Algorithmia.client(apiKey)
+
+
+algo = client.algo('demo/Hello/0.1.1')
+response = algo.pipe("HAL 9000")
+
+input = {
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/de/bd/3d/debd3d0f7478d7a6b0b7215ee779e323.jpg",
+    "numResults": 7
+}
+
+
+
+
+
+algo = client.algo('deeplearning/EmotionRecognitionCNNMBP/0.1.2')
+
+result = algo.pipe(input).result
+
+print(result)
