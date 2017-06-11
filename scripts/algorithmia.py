ba9841d19488ba202ee970b56fb2e43e78c159dd
Sreejith Sreekumar <sreejith2904@gmail.com>
2017-06-11 17:52:26 -0400

    add dropbox link for images

--- scripts/algorithmia.py
+++ scripts/algorithmia.py
@@ -7,8 +7,14 @@ client = Algorithmia.client(apiKey)
 algo = client.algo('demo/Hello/0.1.1')
 response = algo.pipe("HAL 9000")
 
+# input = {
+#     "image": "https://s-media-cache-ak0.pinimg.com/originals/de/bd/3d/debd3d0f7478d7a6b0b7215ee779e323.jpg",
+#     "numResults": 7
+# }
+
+
 input = {
-    "image": "https://s-media-cache-ak0.pinimg.com/originals/de/bd/3d/debd3d0f7478d7a6b0b7215ee779e323.jpg",
+    "image": "dropbox:///images/angry.jpg",
     "numResults": 7
 }
 
