d974e96e94ca6f32aee2c09b8b0ecba6652fd496
Sreejith Sreekumar <sreejith2904@gmail.com>
2017-06-15 16:17:14 -0400

    update emotion script with dropbox upload function

--- scripts/image_emotion.py
+++ scripts/image_emotion.py
@@ -11,6 +11,17 @@ algo = client.algo('deeplearning/EmotionRecognitionCNNMBP/0.1.2')
 access_token="LADJrEFyvzAAAAAAAAAAPmxqPUmb_CQlDVK-iUvXLKyk6DbxuoV_0_UVoqTqZoIa"
 dbx = dropbox.Dropbox(access_token)
 
+def dropbox_image_upload(image_path, dropbox_path):
+    """
+    
+    Arguments:
+    - `local_filepath`:
+    - `dropbox_path`:
+    """
+    with open(image_path, 'rb') as f:
+        dbx.files_upload(f.read(),upload_path,mute=True)
+        print("Uploaded ", file_name)
+
 
 def get_dropbox_files(folder):
     """
@@ -43,7 +54,11 @@ scores = [analyze_image(image) for image in images]
 
 
     
+## sample upload
+file_name='/home/sree/code/emodrive/images/angry.jpg'
+upload_path="/images/angry.jpg"
 
+dropbox_image_upload(file_name, upload_path)
 
     
 
